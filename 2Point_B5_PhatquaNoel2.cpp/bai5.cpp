a56e2cd7c30bb3b14c4f5768101f2c96abed539e
DHHai313 <dinhhonghai3132004@gmail.com>
2024-08-10 18:10:58 +0700

    2 point

---
+++ 2Point_B5_PhatquaNoel2.cpp/bai5.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int n, s;
+  cin >> n >> s;
+  int a[n];
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  int l = 0;
+  int r = 0;
+  int res = 0;
+  int sum = 0;
+  for (int i = 0; i < n; i++) {
+    sum += a[i];
+    while (sum > s) {
+      sum -= a[l];
+      l++;
+    }
+    res = min(res, i - l + 1);
+  }
+  cout << res;
+}
